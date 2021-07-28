860ce896793ef0fe60f8de940305d80ff6640cd9
lauren-kohn <lauren.kohn@soft-adv.com>
2021-07-27 22:22:33 -0500

    commit tree f849cc81c93409e59308f7ab9a518f01432b1c80

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,17 @@ class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
 
+  # This method is a customized version of what is created by
+  # accepts_nested_attributes_for. It adds functionality by
+  # looking for a category before creating one to avoid the
+  # creation of duplicates.
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
       self.post_categories.build(category: category)
+      # self.categories << category
+      # categories<< is a method created by has_many
     end
   end
 
